ebe7a8cdc3f3e2fad97b6f4a360b18b39792f9ae
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-09 16:07:45 +0700

    adding unique email feature for every users

--- api/serializer.py
+++ api/serializer.py
@@ -3,12 +3,25 @@ from django.contrib.auth.models import User
 from book.models import Book
 
 class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password' : {'write_only': True}}
-    
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {'password' : {'write_only': True}}   
+
+    def validate_email(self, data):
+        if User.objects.filter(email=data).exists():
+            raise serializers.ValidationError("This email is already registered.")
+        return data
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'password': 'password do not match.'}) 
+        return data
+
     def create(self, validated_data):
+        validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
 
